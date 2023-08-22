f003504655cf9d314ea24b7fad8923ea5539e43a
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-08-22 22:33:36 +0600

    Add files via upload

---
+++ 1064.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c, d, e;
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e );
+    int count = 0;
+    //float sum = 0.0;
+    //int sub;
+
+    if(a%2==0)
+    {
+        count +=1.0;
+       // sum += a;
+    }
+
+    //sub = count;
+    if(b%2==0)
+    {
+        count +=1.0;
+       // sum += b;
+    }
+
+    if(c%2==0)
+    {
+        count +=1.0;
+        //sum += c;
+    }
+
+    if(d%2==0)
+    {
+        count +=1.0;
+        //sum += d;
+    }
+    if(e%2==0)
+    {
+        count +=1.0;
+        //sum += e;
+    }
+    //float avg = (sum/count);
+
+    printf("%d valores pares\n", count);
+    //printf("%.1f\n", avg);
+
+
+    return 0;
+}
+

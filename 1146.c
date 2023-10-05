bcf0845b71af9fdcde990358f313e31e046e7e24
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-10-05 17:39:28 +0600

    Add files via upload

---
+++ 1146.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int x;
+    while(1){
+        scanf("%d", &x);
+        if(x==0) break;
+        for(int i=1; i<x; i++){
+            printf("%d ",i);
+        }
+        printf("%d\n",x);
+    }
+
+    return 0;
+}
